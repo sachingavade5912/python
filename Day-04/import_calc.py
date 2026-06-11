93c1b1c467d46b3fe942f135d3bfe68177a1383f
Sachin Gavade <sachingavade5912@gmail.com>
2026-06-11 02:58:36 +0000

    Add: import_calc.py

---
+++ Day-04/import_calc.py
@@ -0,0 +1,7 @@
+# import keyword is used to import a module. Here we are importing calculator.py but when importing we just have to give the name of the module
+# we are also using alias
+
+import calculator as calc
+
+# calling the module
+calc
